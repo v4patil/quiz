e42a8553bde7a12c2a05b0310b81bd022d3d5ef2
V4_patil <vibhorbPatil@gmail.com>
2025-02-23 15:26:10 +0530

    add result activity to show the result

--- app/src/main/java/com/example/quizapp/MainActivity.java
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.quizapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -28,14 +32,105 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+
+        // Data Binding
+        binding = DataBindingUtil.setContentView(
+                this,
+                R.layout.activity_main
+        );
+
+        // Resetting the Scores:
         result = 0;
         totalQuestions = 0;
 
-        quizViewModel = new ViewModelProvider(this).get(QuizViewModel.class);
+        // Creating an instance of 'QuizViewModel'
+        quizViewModel = new ViewModelProvider(this)
+                .get(QuizViewModel.class);
 
+        // Displaying the First Question:
         DisplayFirstQuestion();
+
+        binding.buttonNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DisplayNextQuestions();
+            }
+        });
+
+
+    }
+
+    public void DisplayNextQuestions(){
+
+        // Direct the user to the Results activity
+        if (binding.buttonNext.getText().equals("Finish")){
+            Intent i = new Intent(MainActivity.this, ResultsActivity.class);
+            startActivity(i);
+            finish();
+        }
+
+
+        // Displaying the question
+        int selectedOption = binding.radioGroup.getCheckedRadioButtonId();
+        if (selectedOption != -1){
+            RadioButton radioButton = findViewById(selectedOption);
+
+            // More Questions to Display??
+            if ((questionList.size() -i) > 0){
+
+                // Getting the number of questions
+                totalQuestions = questionList.size();
+
+                // Check if the chosen option is correct
+                if(radioButton.getText().toString().equals(
+                        questionList.get(i).getCorrectOption()
+                )){
+                    result++;
+                    binding.txtResult.setText(
+                            "Correct Answers: "+result
+                    );
+                }
+
+                if (i ==0){
+                    i++;
+                }
+
+                // Displaying the next Questions
+                binding.txtQuestion.setText("Question "+(i+1)+ " : "+
+                        questionList.get(i).getQuestion());
+
+                binding.radio1.setText(questionList.get(i).getOption1());
+                binding.radio2.setText(questionList.get(i).getOption2());
+                binding.radio3.setText(questionList.get(i).getOption3());
+                binding.radio4.setText(questionList.get(i).getOption4());
+
+
+                // Check if it is the last question
+                if(i == (questionList.size() -1)){
+                    binding.buttonNext.setText("Finish");
+                }
+
+                binding.radioGroup.clearCheck();
+                i++;
+
+
+            }else{
+                if (radioButton.getText().toString().equals(
+                        questionList.get(i-1).getCorrectOption()
+                )){
+                    result++;
+                    binding.txtResult.setText("Correct Answers : "+result);
+                }
+
+            }
+        }else{
+            Toast.makeText(
+                    this,
+                    "You need to make a selection",
+                    Toast.LENGTH_SHORT).show();
+        }
+
     }
 
     private void DisplayFirstQuestion() {
